fcf7b1af5909cd212ce8bd707880ef41cf2b87c1
amon <amonamanya17@gmail.com>
2026-04-28 17:06:40 +0200

    working version: fastapi + rag + openai embeddings

--- embedding.py
+++ embedding.py
@@ -1,5 +1,9 @@
 #from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
+
+# def get_embedding_function():
+#     return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+from langchain_openai import OpenAIEmbeddings
 
 def get_embedding_function():
-    return HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return OpenAIEmbeddings(model="text-embedding-3-small")
